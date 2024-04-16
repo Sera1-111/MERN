99ea3793147dde1c0f4461b18c2b9bdb589dc08d
Sera1-111 <cazelleevoni@gmail.com>
2024-04-16 13:09:10 +0530

    signup error resolved

--- frontend/src/CustomHook/UseSignup.js
+++ frontend/src/CustomHook/UseSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
         setError(null);
 
         try {
-            const response = await fetch('https://mern-1ogg.onrender.comapi/user/signup', {
+            const response = await fetch('https://mern-1ogg.onrender.com/api/user/signup', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({ username, email, password }) // Modified body to include username
